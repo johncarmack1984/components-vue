9a34e928a7e55f076dc35864bc86d009c01041d3
John Carmack <johncarmack@me.com>
2023-09-29 21:19:27 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script lang="ts" setup>
+import uiButton from './components/ui-button.vue'
+</script>
+<template>
+  <ui-button>My Button</ui-button>
+</template>
---
+++ src/components/ui-button.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+const handleClick = () => {
+  alert('Button clicked')
+}
+</script>
+
+<template>
+  <button
+    @click="handleClick"
+    id="ui-button"
+    class="rounded-[5px] border-none p-[10px] text-white bg-[blue]"
+  >
+    <slot />
+  </button>
+</template>
